d531ce597b3fc1f06fa4f121d2e9cabb7742c0a9
kimu_shu <alfvegardrisc@gmail.com>
2017-07-07 23:04:26 +0900

    Add zero filler

--- ip/peridot_hostbridge/tools/elf.tcl
+++ ip/peridot_hostbridge/tools/elf.tcl
@@ -236,9 +236,12 @@ proc ELF_compact { varName procName type } {
 			# Skip this segment
 			continue
 		}
+		
+		# Make filler
+		set filler [ string repeat { } [ expr $phdr(p_memsz) - $phdr(p_filesz) ] ]
 
 		# Try compression
-		set result [ $procName $phdr(content) ]
+		set result [ $procName $phdr(content)$filler ]
 		set newlen [ string length $result ]
 		if { ($newlen == 0) || ($newlen >= $phdr(p_filesz)) } {
 			# Compression error / Enlarged by compression :-(
